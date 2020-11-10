879d59f7c9bffd34f0b5554d39119dbd852aefd3
Alexander Burdiss <aburdiss@gmail.com>
2020-11-09 22:48:02 -0500

    Added Japanese language support

--- TubaRoutines/List View/ExerciseDetailView.swift
+++ TubaRoutines/List View/ExerciseDetailView.swift
@@ -49,7 +49,7 @@ struct ExerciseDetailView: View {
             }
             .padding()
         )
-    .navigationViewStyle(StackNavigationViewStyle())
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
